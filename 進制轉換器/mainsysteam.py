8fee1d9054dbf53ae0d82e65ad6207901a98d5a5
ZombieEnderman <p0966959620@gmail.com>
2024-12-05 01:03:38 +0800

    進制轉換器0.1版: 完成基本功能。

--- 進制轉換器/mainsysteam.py
+++ 進制轉換器/mainsysteam.py
@@ -9,34 +9,45 @@ from tkinter import *
 def binary():
     """ 轉二進位 """
 
+    var=bin(int(input_value.get()))
+    result_txt.config(text=f'二進位:\n{str(var)[2:]}')
+
 
 def octal():
     """ 轉八進位 """
+
+    var=oct(int(input_value.get()))
+    result_txt.config(text=f'八進位:\n{str(var)[2:]}')
 
 
 def hexadecimal():
     """ 轉十六進位 """
 
+    var=hex(int(input_value.get()))
+    result_txt.config(text=f'十六進位:\n{str(var)[2:]}')
+
 
 # 系統區
 window=Tk()                     #視窗本體
-window.title('進制轉換器')       #標題
+window.title('進制轉換器')      #標題
 window.geometry('720x520')      #大小
 window.config(bg='#000000')     #背景顏色
 
 hint_txt=Label(window,text='輸入十進位數字後按下按鈕',bg='#000000',fg='#00ff00',font=('新細明體',20,'bold'))    #操作提示文字
-hint_txt.place(x=200,y=10)                                                                                   #操作提示文字之布局
-value_txt=Label(window,text='十進位',bg='#000000',fg='#00ff00',font=('新細明體',20,'bold'))                   #輸入框提示文字
-value_txt.place(x=170,y=100)                                                                                 #輸入框提示文字之布局
+hint_txt.place(x=200,y=10)                                                                                      #操作提示文字之布局
+value_txt=Label(window,text='十進位',bg='#000000',fg='#00ff00',font=('新細明體',20,'bold'))                     #輸入框提示文字
+value_txt.place(x=170,y=250)                                                                                    #輸入框提示文字之布局
+result_txt=Label(window,text='',bg='#000000',fg='#00ff00',font=('新細明體',30,'bold'))                          #結果文字
+result_txt.place(x=250,y=100)                                                                                   #結果文字之布局
 
 input_value=Entry(window,bg='#ffffff',fg='#ff0000',font=('新細明體',20,'bold'))     #輸入框
-input_value.place(x=260,y=100)                                                     #輸入框之布局
+input_value.place(x=260,y=250)                                                      #輸入框之布局
 
-bin_button=Button(window,text='轉二進位',bg='#ffffff',fg='#000000',font=('新細明體',15,'bold'))         #二進位鈕
-bin_button.place(x=150,y=140)                                                                         #二進位鈕之布局
-oct_button=Button(window,text='轉八進位',bg='#ffffff',fg='#000000',font=('新細明體',15,'bold'))         #八進位鈕
-oct_button.place(x=260,y=140)                                                                         #八進位鈕之布局
-hex_button=Button(window,text='轉十六進位',bg='#ffffff',fg='#000000',font=('新細明體',15,'bold'))       #十六進位鈕
-hex_button.place(x=370,y=140)                                                                         #十六進位鈕之布局
+bin_button=Button(window,text='轉二進位',bg='#ffffff',fg='#000000',font=('新細明體',15,'bold'),command=binary)         #二進位鈕
+bin_button.place(x=200,y=350)                                                                                          #二進位鈕之布局
+oct_button=Button(window,text='轉八進位',bg='#ffffff',fg='#000000',font=('新細明體',15,'bold'),command=octal)          #八進位鈕
+oct_button.place(x=310,y=350)                                                                                          #八進位鈕之布局
+hex_button=Button(window,text='轉十六進位',bg='#ffffff',fg='#000000',font=('新細明體',15,'bold'),command=hexadecimal)  #十六進位鈕
+hex_button.place(x=420,y=350)                                                                                          #十六進位鈕之布局
 
 window.mainloop()
