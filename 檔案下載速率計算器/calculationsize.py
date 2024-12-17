c533e9e68061b60e514ad64ea363c5a3bb382371
ZombieEnderman <p0966959620@gmail.com>
2024-12-17 22:29:42 +0800

    完成附屬模組之功能。

--- 檔案下載速率計算器/calculationsize.py
+++ 檔案下載速率計算器/calculationsize.py
@@ -1,20 +1,54 @@
 """
 這是計算大小模組!\n
-本模組用於計算檔案大小與傳輸速率。\n
+本模組用於計算檔案大小、傳輸速率、時間。\n
 """
 
-capacity={'KB':1_024 * 8,
-          'MB':1_024 * 1_024 * 8,
-          'GB':1_024 * 1_024 * 1_024 * 8,
-          'TB':1_024 * 1_024 * 1_024 * 1_024 * 8,
-          'PB':1_024 * 1_024 * 1_024 * 1_024 * 1_024 * 8,
-          'EB':1_024 * 1_024 * 1_024 * 1_024 * 1_024 * 1_024 * 8,
-          'ZB':1_024 * 1_024 * 1_024 * 1_024 * 1_024 * 1_024 * 1_024 * 8}#檔案大小字典
+file_size=None  #檔案大小暫存器
+velocity=None   #傳輸速率暫存器
+time=None       #時間暫存器
 
-transmission={'Kbps':1_000,
-              'Mbps':1_000 * 1_000,
-              'Gbps':1_000 * 1_000 * 1_000,
-              'Tbps':1_000 * 1_000 * 1_000 * 1_000,
-              'Pbps':1_000 * 1_000 * 1_000 * 1_000 * 1_000,
-              'Ebps':1_000 * 1_000 * 1_000 * 1_000 * 1_000 * 1_000,
-              'Zbps':1_000 * 1_000 * 1_000 * 1_000 * 1_000 * 1_000 * 1_000}#傳輸速率字典
+capacity={'KB':pow(2,13),
+          'MB':pow(2,23),
+          'GB':pow(2,33),
+          'TB':pow(2,43),
+          'PB':pow(2,53),
+          'EB':pow(2,63),
+          'ZB':pow(2,73)}           #檔案大小字典
+
+transmission={'Kbps':pow(10,3),
+              'Mbps':pow(10,6),
+              'Gbps':pow(10,9),
+              'Tbps':pow(10,12),
+              'Pbps':pow(10,15),
+              'Ebps':pow(10,18),
+              'Zbps':pow(10,21)}    #傳輸速率字典
+
+def calculate_size(unit,value):
+    """ 計算檔案大小 """
+
+    global file_size
+    file_size = capacity[unit] * value
+
+
+def calculate_speed(unit,value):
+    """ 計算傳輸速率 """
+
+    global velocity
+    velocity = transmission[unit] * value
+
+
+def calculate_time(second):
+    """ 計算時間 """
+
+    global time
+    if second >= 3_600:
+        hour = second // 3_600
+        minute = second % 3_600
+        second = minute % 60
+        time = (hour,minute,second)
+    elif 60 <= second < 3_600:
+        minute = second // 60
+        second %= 60
+        time = (0,minute,second)
+    else:
+        time = (0,0,second)
