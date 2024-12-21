fb6b3cc6f2152040046508b1a64e83983be9c17e
ZombieEnderman <p0966959620@gmail.com>
2024-12-21 16:42:52 +0800

    working...

--- 檔案下載速率計算器/mainsystem.py
+++ 檔案下載速率計算器/mainsystem.py
@@ -34,33 +34,43 @@ def display(x):
 
     result_txt.config(text=f'所需時間:{x}秒')
 
-# 系統區
-window=Tk()                           #視窗本體
+# 視窗區
+window = Tk()                         #本體
 window.title('檔案下載速率計算器')    #標題
 window.geometry('720x520')            #大小
 window.config(bg='#ffffff')           #背景顏色
 
-hint_txt1 = Label(window,text='1.輸入大小與速率',bg='#ffffff',fg='#000000',font=('標楷體',20,'bold'))       #操作提示文字1
-hint_txt1.place(x=240,y=10)                                                                                #操作提示文字1之布局
-hint_txt2 = Label(window,text='2.選取單位',bg='#ffffff',fg='#000000',font=('標楷體',20,'bold'))             #操作提示文字2
-hint_txt2.place(x=240,y=60)                                                                                #操作提示文字2之布局
-hint_txt3 = Label(window,text='3.按下"計算"之按鈕',bg='#ffffff',fg='#000000',font=('標楷體',20,'bold'))     #操作提示文字3
-hint_txt3.place(x=240,y=110)                                                                               #操作提示文字3之布局
-input_txt1 = Label(window,text='檔案大小',bg='#ffffff',fg='#000000',font=('標楷體',20,'bold'))              #輸入框提示文字1
-input_txt1.place(x=150,y=300)                                                                              #提示文字1之布局
-input_txt2 = Label(window,text='傳輸速率',bg='#ffffff',fg='#000000',font=('標楷體',20,'bold'))              #輸入框提示文字2
-input_txt2.place(x=150,y=350)                                                                              #提示文字2之布局
-result_txt = Label(window,text='所需時間:',bg='#ffdd00',fg='#000000',font=('標楷體',20,'bold'))             #結果文字
-result_txt.place(x=260,y=200)                                                                              #結果文字之佈局
+# 容器區
+capacity_tuple = tuple(z for z in csize.capacity)           #檔案大小元組
+transmission_tuple = tuple(z for z in csize.transmission)   #傳輸速率元組
 
-box1 = Entry(window,bg='#ffffff',fg='#000000',font=('Arial',20,'bold'))       #檔案大小輸入框
-box1.place(x=270,y=300)                                                       #檔案大小輸入框之布局
-box2 = Entry(window,bg='#ffffff',fg='#000000',font=('Arial',20,'bold'))       #檔案大小輸入框
-box2.place(x=270,y=350)                                                       #檔案大小輸入框之布局
+# 文字標籤區
+input_txt1 = Label(window, text='檔案大小', bg='#ffffff', fg='#000000', font=('標楷體', 20, 'bold'))         #檔案標籤
+input_txt1.place(x=150,y=170)                                                                                #檔案標籤之布局
+input_txt2 = Label(window, text='傳輸速率', bg='#ffffff', fg='#000000', font=('標楷體', 20, 'bold'))         #速率標籤
+input_txt2.place(x=150,y=300)                                                                                #速率標籤之布局
+result_txt = Label(window, text='所需時間:', bg='#ffdd00', fg='#000000', font=('標楷體', 20, 'bold'))        #結果標籤
+result_txt.place(x=300,y=50)                                                                                 #結果標籤之佈局
 
-calculate = Button(window,text='計算',bg='#999999',fg='#ff0000',font=('新細明體',25,'bold'),command=calculate_download)    #計算鈕
-calculate.place(x=367,y=430)                                                                                              #計算鈕之布局
-clear = Button(window,text='清空',bg='#999999',fg='#ff0000',font=('新細明體',25,'bold'),command=clearall)                  #完全清空鈕
-clear.place(x=270,y=430)                                                                                                  #完全清空鈕之布局
+# 輸入框區
+box1 = Entry(window, bg='#ffffff', fg='#000000', font=('Arial', 20, 'bold'))    #檔案輸入框
+box1.place(x=270,y=170)                                                         #檔案輸入框之布局
+box2 = Entry(window, bg='#ffffff', fg='#000000', font=('Arial', 20, 'bold'))    #速率輸入框
+box2.place(x=270,y=300)                                                         #速率輸入框之布局
+
+# 單選鈕區
+for z in range(len(capacity_tuple)):        #檔案選擇鈕
+    capacity = Radiobutton(window, text=capacity_tuple[z], bg='#ffffff', variable=IntVar(), value=z)
+    capacity.place(x=260+(z*45), y=230)
+
+for z in range(len(transmission_tuple)):    #速率選擇鈕
+    transmission = Radiobutton(window, text=transmission_tuple[z], bg='#ffffff', variable=IntVar(), value=z)
+    transmission.place(x=190+(z*55), y=350)
+
+# 按鈕區
+calculate = Button(window, text='計算', bg='#999999', fg='#ff0000', font=('新細明體',25,'bold'), command=calculate_download)    #計算鈕
+calculate.place(x=367,y=430)                                                                                                    #計算鈕之布局
+clear = Button(window, text='清空', bg='#999999', fg='#ff0000', font=('新細明體',25,'bold'), command=clearall)                  #清空鈕
+clear.place(x=270,y=430)                                                                                                        #清空鈕之布局
 
 window.mainloop()
