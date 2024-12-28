de0d256c217fe4ad4a8dc7da8042b2e829f3067f
ZombieEnderman <p0966959620@gmail.com>
2024-12-28 17:40:35 +0800

    working to "天干地支年份計算器"...

---
+++ 天干地支年份計算器/hseb.py
@@ -0,0 +1,18 @@
+"""
+這是天干地支模組!\n
+本模組用於計算天干地支年份。\n
+"""
+
+heavenly_stems = '甲乙丙丁戊己庚辛壬癸'          #天干
+earthly_branches = '子丑寅卯辰巳午未申酉戌亥'    #地支
+base_year = 1984                                #基準年(甲子年)
+
+def calculate_HS(year):
+    """ 計算天干 """
+
+    pass
+
+def calculate_EB(year):
+    """ 計算地支 """
+
+    pass
---
+++ 天干地支年份計算器/mainsystem.py
@@ -0,0 +1,22 @@
+"""
+這是天干地支年份計算器主系統!
+本系統用於整合其他附屬模組。
+"""
+
+# 模組區
+from tkinter import *
+import hseb
+
+# 視窗區
+window = Tk()
+window.title('天干地支年份計算器')
+window.geometry('720x520')
+window.config(bg='#ffff00')
+
+# 文字標籤區
+
+# 輸入框區
+
+# 按鈕區
+
+window.mainloop()
