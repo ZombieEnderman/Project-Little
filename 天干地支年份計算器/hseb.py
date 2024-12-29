d55a2e066e1bbeef9d54b657a71df196c32b0618
ZombieEnderman <p0966959620@gmail.com>
2024-12-29 20:46:51 +0800

    天干地支年份計算器0.1版:完成基本功能。

--- 天干地支年份計算器/hseb.py
+++ 天干地支年份計算器/hseb.py
@@ -3,16 +3,19 @@
 本模組用於計算天干地支年份。\n
 """
 
-heavenly_stems = '甲乙丙丁戊己庚辛壬癸'          #天干
-earthly_branches = '子丑寅卯辰巳午未申酉戌亥'    #地支
-base_year = 1984                                #基準年(甲子年)
+heavenly_stems = '甲乙丙丁戊己庚辛壬癸'          #天干字串
+earthly_branches = '子丑寅卯辰巳午未申酉戌亥'    #地支字串
+year_base = 1984                                #基準年(甲子年)
 
 def calculate_HS(year):
     """ 計算天干 """
 
-    pass
+    year_difference = year - year_base
+    return heavenly_stems[year_difference % len(heavenly_stems)]
+
 
 def calculate_EB(year):
     """ 計算地支 """
 
-    pass
+    year_difference = year - year_base
+    return earthly_branches[year_difference % len(earthly_branches)]
--- 天干地支年份計算器/mainsystem.py
+++ 天干地支年份計算器/mainsystem.py
@@ -7,16 +7,49 @@
 from tkinter import *
 import hseb
 
+def calculate():
+    """ 計算天干地支 """
+
+    try:
+        year = int(year_box.get())
+        result = hseb.calculate_HS(year) + hseb.calculate_EB(year) + '年'
+        display(result)
+    except:
+        display('請輸入\n有效數值!')
+
+
+def display(x=''):
+    """ 顯示結果 """    
+
+    result_txt.config(text=x)
+
+
+def clear():
+    """ 清空輸入框 """
+
+    year_box.delete(0,END)
+    display()
+
 # 視窗區
 window = Tk()
 window.title('天干地支年份計算器')
-window.geometry('720x520')
+window.geometry('520x420')
 window.config(bg='#ffff00')
 
 # 文字標籤區
+result_txt = Label(window, text='', bg='#ffff00', fg='#000000', font=('標楷體', 40, 'bold'))
+result_txt.place(x=180, y=80)
+year_txt = Label(window, text='西元年份:', bg='#ffff00', fg='#000000', font=('新細明體', 25, 'bold'))
+year_txt.place(x=100, y=200)
 
 # 輸入框區
+year_box = Entry(window, bg='#ffffff', fg='#000000', font=('標楷體', 20, 'bold'), width=10)
+year_box.place(x=250, y=200)
 
 # 按鈕區
+calculate_button = Button(window, text='計算', bg='#00ff00', fg='#000000', font=('新細明體', 25, 'bold'), command=calculate)
+calculate_button.place(x=150, y=250)
+clear_button = Button(window, text='清空', bg='#00ff00', fg='#000000', font=('新細明體', 25, 'bold'), command=clear)
+clear_button.place(x=250, y=250)
 
 window.mainloop()
