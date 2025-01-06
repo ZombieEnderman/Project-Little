8dfdcc5d9dada339a7a4ec81eefb286a8026c1de
ZombieEnderman <p0966959620@gmail.com>
2025-01-06 11:44:34 +0800

    pow(n, m) -> n ** m

--- 檔案下載速率計算器/calculationsize.py
+++ 檔案下載速率計算器/calculationsize.py
@@ -7,21 +7,21 @@ file_size=None  #檔案大小暫存器
 velocity=None   #傳輸速率暫存器
 time=None       #時間暫存器
 
-capacity={'KB':pow(2,13),
-          'MB':pow(2,23),
-          'GB':pow(2,33),
-          'TB':pow(2,43),
-          'PB':pow(2,53),
-          'EB':pow(2,63),
-          'ZB':pow(2,73)}           #檔案大小字典
+capacity={'KB':2 ** 13,
+          'MB':2 ** 23,
+          'GB':2 ** 33,
+          'TB':2 ** 43,
+          'PB':2 ** 53,
+          'EB':2 ** 63,
+          'ZB':2 ** 73}           #檔案大小字典
 
-transmission={'Kbps':pow(10,3),
-              'Mbps':pow(10,6),
-              'Gbps':pow(10,9),
-              'Tbps':pow(10,12),
-              'Pbps':pow(10,15),
-              'Ebps':pow(10,18),
-              'Zbps':pow(10,21)}    #傳輸速率字典
+transmission={'Kbps':10 ** 3,
+              'Mbps':10 ** 6,
+              'Gbps':10 ** 9,
+              'Tbps':10 ** 12,
+              'Pbps':10 ** 15,
+              'Ebps':10 ** 18,
+              'Zbps':10 ** 21}    #傳輸速率字典
 
 def calculate_size(unit,value):
     """ 計算檔案大小 """
