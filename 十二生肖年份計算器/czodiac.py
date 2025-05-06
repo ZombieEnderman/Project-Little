d7f2abebfc70ef5edb457ed093aff6eb85dab4af
ZombieEnderman <p0966959620@gmail.com>
2025-05-06 21:39:46 +0800

    十二生肖年份計算器0.2: 修正模組錯誤。

--- 十二生肖年份計算器/czodiac.py
+++ 十二生肖年份計算器/czodiac.py
@@ -7,16 +7,20 @@ zodiac = '鼠牛虎兔龍蛇馬羊猴雞狗豬'     # 十二生肖
 base_year = 2020                       # 鼠年
 register = None                        # 年份暫存器
 
+
 def calculate_zodiac(x):
     """ 計算生肖 """
 
     global register
-    register = abs(x - base_year)
+    register = x - base_year
     register %= len(zodiac)
     return zodiac[register] + '年'
 
+
 if __name__ == '__main__':
     print(calculate_zodiac(2020))
+    print(calculate_zodiac(2003))
+    print(calculate_zodiac(2004))
     print(calculate_zodiac(2024))
     print(calculate_zodiac(2025))
     print(calculate_zodiac(1912))
