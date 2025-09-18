83b67c87c563ed82a51af48bc8f29c0bf3be7744
ZombieEnderman <p0966959620@gmail.com>
2025-09-18 22:07:41 +0800

    虛擬骰子0.2版: 1.更新介面 2.模組化系統 3.新增型別註解 4.新增多種骰子

--- 虛擬骰子/mainsystem.py
+++ 虛擬骰子/mainsystem.py
@@ -8,24 +8,39 @@ from dice import *
 from tkinter import *  # type: ignore
 
 
-def sides():
-    """ 擲骰子 """
-    result = scope(1, 6)
-    txt.config(text=result, font=('Arial', 50, 'bold'))
+def refresh_screen() -> None:
+    """刷新畫面"""
+    dice_map = {
+        "4面": four_sides,
+        "6面": six_sides,
+        "8面": eight_sides,
+        "10面": ten_sides,
+        "12面": twelve_sides,
+        "20面": twenty_sides,
+    }
+    func = dice_map.get(var.get(), six_sides)
+    result = str(func()[0])
+    txt.config(text=result, font=('Arial', 40, 'bold'))
 
 
 # 系統區
-window = Tk()
+window: Tk = Tk()
 window.title('虛擬骰子')
-window.geometry('720x520')
-window.config(bg='#000000')
+window.geometry('520x320')
+window.config(bg="#FFFFFF")
+window.iconbitmap("dice.ico")
 
-txt = Label(window, text='按下按鈕來擲骰子!', bg='#000000',
-            fg='#00ff00', font=('標楷體', 30, 'bold'))
-txt.pack(side='top')
+txt = Label(window, text='按下按鈕來擲骰子!', bg="#FFFFFF",
+            fg="#000000", font=('標楷體', 30, 'bold'))
+txt.pack(side='top', pady=5)
 
-run = Button(window, text='擲骰子', bg='#ffffff', fg='#ff0000',
-             font=('標楷體', 30, 'bold'), command=sides)
-run.pack(side='bottom')
+Button(window, text='擲骰子', bg="#e5ff00", fg='#ff0000', font=(
+    '標楷體', 30, 'bold'), command=refresh_screen).pack(side='bottom', pady=10)
+
+var = StringVar(value="6面")
+array = ("4面", "6面", "8面", "10面", "12面", "20面")
+for index in range(len(array)):
+    Radiobutton(window, variable=var,
+                value=array[index], text=array[index], font=("標楷體", 16, "bold")).pack(side='left', padx=15)
 
 window.mainloop()
